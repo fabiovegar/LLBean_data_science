4c0b9e5f0be568659f479eb0a3117393036d4330
fabiovegar <fabiovegar@gmail.com>
2019-05-12 23:06:37 -0600

    Ejercicios para session2

--- Tareas/Tarea 3.py
+++ Tareas/Tarea 3.py
@@ -9,25 +9,24 @@ persona= [('Sofia',   'Alfaro',   32415116,   36 , 51161161, 'consulta'),
           ('Melissa', 'Alvarado', 734114144,  10 , 87421312, 'dolor'),
           ('Pedro',   'Castro',   4372124141, 2 ,  99313131, 'dolor')]
 agenda_hosp.extend(persona)
-for paciente in agenda_hosp:
-    print (paciente)
+
 
 #Primera pregunta Cuantos pacientes llegaron en total?
-print('Llegaron %i pacientes!'%(len(agenda_hosp)))
+print('#1.Llegaron %i pacientes!'%(len(agenda_hosp)))
 
 #Segunda pregunta Cuantas personas llegaron por dolor?
 cant=0
 for i in range (len(agenda_hosp)):
     for j in range (len(agenda_hosp[i])):
-        a=agenda_hosp[i][j]
-    if(a=='dolor'):
-        cant+=1
-print('Llegaron %i pacientes con dolor.'%cant)
+        if(agenda_hosp[i][j]=='dolor'):
+            cant+=1
+print('#2.Llegaron %i pacientes con dolor.'%cant)
 
 
 #Tercera pregunta Suponga que se atienden con orden de prioridad por edad, empezando por el adulto mayor.
 #Ordene la lista desde el más adulto al más joven
-
+lista1= sorted(agenda_hosp, key=lambda i:i[3],reverse=True)
+print ('#3.',lista1)
 
 
 #Cuarta Pregunta
@@ -41,7 +40,7 @@ for i in range (len(agenda_hosp)):
     else:
         menores+=1
 
-print('Hay %i pacientes mayores de edad y %i menores de edad'%(mayores,menores))
+print('#4.Hay %i pacientes mayores de edad y %i menores de edad'%(mayores,menores))
 
 
 
@@ -49,5 +48,25 @@ print('Hay %i pacientes mayores de edad y %i menores de edad'%(mayores,menores))
 #Suponga que se atienden con orden de prioridad por gravedad de consulta, empezando por los que tienen dolor
 #y luego por edad (mas viejo al joven), empezando por el adulto mayor. Ordene la lista empenzando por los que tienen mayor prioridad.
 
+lista2=[]
+lista3=[]
+for i in range (len(agenda_hosp)):
+    if agenda_hosp[i][5]=='dolor':
+        lista2.append(agenda_hosp[i])
+    else:
+        lista3.append(agenda_hosp[i])
+lista_dolor=sorted(lista2, key=lambda x:x[3],reverse=True)
+lista_otros=sorted(lista3, key=lambda x:x[3],reverse=True)
+lista_final=lista_dolor+lista_otros
+print('#5.',lista_final)
+
+
+
 #Sexta pregunta
 #Suponga que los que tienen dolor mueren :( Como queda la lista de pacientes vivos por atender ordenados por orden de edad desde el joven al viejo.
+new=[]
+for i in range (len(agenda_hosp)):
+    if agenda_hosp[i][5]!='dolor':
+        new.append(agenda_hosp[i])
+print('#6.',(sorted(new, key=lambda x:x[3])))
+
