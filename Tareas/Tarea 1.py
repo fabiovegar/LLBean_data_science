ed2be0d0ab6654bc3b442acd35fd6106533858a2
fabiovegar <fabiovegar@gmail.com>
2019-05-14 18:00:14 -0600

    Ejercicios para session2

--- Tareas/Tarea 1.py
+++ Tareas/Tarea 1.py
@@ -1,5 +1,4 @@
 pi=3.1415926
 a='*'
 
-print('%f%s%.5f%s%.4f%s%.3f%s%.2f%s' %(pi,a,pi,a*2,pi,a*3,pi,a*4,pi,a*5))
-
+print('%s%.7f%s%.6f%s%.5f%s%.4f%s%.3f%s%.2f'%(a,pi,a*2,pi,a*3,pi,a*4,pi,a*5,pi,a*6,pi))
